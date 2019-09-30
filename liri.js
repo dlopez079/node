6c537216a231672e7d47f9b5b868714cbf8f46d7
David Lopez <dlopez079@gmail.com>
2019-09-30 19:25:17 -0400

    Finished the movieThis function.

--- liri.js
+++ liri.js
@@ -8,13 +8,13 @@ var fs = require("fs");
 
 
 var whatToDo = process.argv[2];
-var userInput = process.argv[3];
+var userInput = process.argv.slice(3).join(" ");
 
 console.log("LIRI.JS LOADED SUCCESSFULLY!")
 
 
 //SWITCH STATMENTS*******************
-switch (whatToDo) {
+switch (whatToDo) { //
     case "concert-this":
         concertThis();
         break;
@@ -45,32 +45,41 @@ function concertThis() {
     //Console.log successful load.
     console.log("CONCERT.THIS FUNCTION LOADED SUCCESSFULLY!");
 
-// Then run a request with axios to the OMDB API with the movie specified
-axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
-  function(response) {
-    console.log("SEARCHING FOR: " + userInput);
-    console.log(response);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  }); 
+    // Then run a request with axios to the OMDB API with the movie specified
+    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log("SEARCHING FOR: " + userInput);
+            // console.log(response.data);
+            console.log("****************************")
+            for (i = 0; i < response.data.length; i++) {
+                console.log("VENUE NAME: " + response.data[i].venue.name);
+                console.log("VENUE CITY: " + response.data[i].venue.city);
+                console.log("DATE: " + response.data[i].datetime);
+                
+                console.log("****************************")
+            }
+
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 }// END OF CONCERTTHIS FUNCTION******************************
 
 
@@ -81,7 +90,10 @@ function spotifyThisSong() {
     spotify
         .search({ type: 'track', query: userInput, limit: 20 })
         .then(function (response) {
-            console.log(JSON.stringify(response.tracks.items[0].name, null, 2));
+            console.log("SONG: " + JSON.stringify(response.tracks.items[0].name, null, 2));
+            console.log("ARTIST: " + JSON.stringify(response.tracks.items[0].artists[0].name, null, 2))
+            console.log("URL: " + JSON.stringify(response.tracks.items[0].preview_url, null, 2));
+
         })
         .catch(function (err) {
             console.log(err);
@@ -92,7 +104,39 @@ function spotifyThisSong() {
 
 //ESTABLISH MOVIETHIS FUNCTION********************************
 function movieThis() {
+   // Then run a request with axios to the OMDB API with the movie specified
+axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(
+    function(response) {
+      console.log("Title: " + response.data.Title);
+      console.log("Year: " + response.data.Year);
+      console.log("Rating: " + response.data.Ratings[0].Value);
+      console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+      console.log("Country: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
 
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    }); 
 } // END OF MOVIETHIS FUNCTION********************************
 
 
